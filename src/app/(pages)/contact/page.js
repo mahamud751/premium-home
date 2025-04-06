9b59dea638b7b010e38341a5a9b4cb7ecf3ea4cb
mdsajalali <mdsajalali2020@gmail.com>
2025-04-06 21:36:16 +0600

    added: contact form ui

--- src/app/(pages)/contact/page.js
+++ src/app/(pages)/contact/page.js
@@ -2,8 +2,7 @@ import CallToActions from "@/components/common/CallToActions";
 import DefaultHeader from "@/components/common/DefaultHeader";
 import Footer from "@/components/common/default-footer";
 import MobileMenu from "@/components/common/mobile-menu";
-import Form from "@/components/pages/contact/Form";
-import Office from "@/components/pages/contact/Office";
+import ContactForm from "@/components/pages/contact/ContactForm";
 
 export const metadata = {
   title: "Contact  || Homez - Real Estate NextJS Template",
@@ -25,7 +24,7 @@ const Contact = () => {
         <iframe
           className="home8-map contact-page"
           loading="lazy"
-          src="https://maps.google.com/maps?q=London%20Eye%2C%20London%2C%20United%20Kingdom&t=m&z=14&output=embed&iwloc=near"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.2869934920495!2d90.36561957592886!3d23.80839158654639!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c1004c51b2db%3A0x254957df1d4af9b7!2sMirpur%2010%20Metro%20Station!5e0!3m2!1sen!2sbd!4v1743948686411!5m2!1sen!2sbd"
           title="London Eye, London, United Kingdom"
           aria-label="London Eye, London, United Kingdom"
         />
@@ -33,63 +32,14 @@ const Contact = () => {
       {/* End Our Contact With Map */}
 
       {/* Start Our Contact Form */}
-      <section>
-        <div className="container">
-          <div className="row d-flex align-items-end">
-            <div className="col-lg-5 position-relative">
-              <div className="home8-contact-form default-box-shadow1 bdrs12 bdr1 p30 mb30-md bgc-white">
-                <h4 className="form-title mb25">
-                  Have questions? Get in touch!
-                </h4>
-                <Form />
-              </div>
-            </div>
-            {/* End .col */}
-
-            <div className="col-lg-5 offset-lg-2">
-              <h2 className="mb30 text-capitalize">
-                We’d love to hear <br className="d-none d-lg-block" />
-                from you.
-              </h2>
-              <p className="text">
-                We are here to answer any question you may have. As a partner of
-                corporates, realton has more than 9,000 offices of all sizes and
-                all potential of session.
-              </p>
-            </div>
-            {/* End .col */}
-          </div>
-        </div>
-      </section>
+      <section></section>
       {/* End Our Contact Form */}
 
-      {/* Visit our Office */}
-      <section className="pt0 pb90 pb10-md">
-        <div className="container">
-          <div className="row">
-            <div
-              className="col-lg-6 m-auto"
-              data-aos="fade-up"
-              data-aos-delay="300"
-            >
-              <div className="main-title text-center">
-                <h2 className="title">Visit Our Office</h2>
-                <p className="paragraph">
-                  Realton has more than 9,000 offices of all sizes and all
-                  potential of session.
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* End .row */}
-
-          <div className="row" data-aos="fade-up" data-aos-delay="100">
-            <Office />
-          </div>
-          {/* End .row */}
-        </div>
+      {/* Contact form start here */}
+      <section className="container">
+        <ContactForm />
       </section>
-      {/* End Visit our Office */}
+      {/* Contact form end here */}
 
       {/* Our CTA */}
       <CallToActions />
