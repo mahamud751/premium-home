8250e1f170c3070c31017a7e7b910526194d1ca1
mdsajalali <mdsajalali2020@gmail.com>
2025-04-06 20:03:55 +0600

    added: about us content ui

--- src/app/(pages)/about/page.js
+++ src/app/(pages)/about/page.js
@@ -3,11 +3,7 @@ import DefaultHeader from "@/components/common/DefaultHeader";
 import Partner from "@/components/common/Partner";
 import Footer from "@/components/common/default-footer";
 import MobileMenu from "@/components/common/mobile-menu";
-import Agents from "@/components/pages/about/Agents";
-import Features from "@/components/pages/about/Features";
-import FunFact from "@/components/pages/about/FunFact";
-import Mission from "@/components/pages/about/Mission";
-import Image from "next/image";
+import AboutContent from "@/components/pages/about/AboutContent";
 import Link from "next/link";
 
 export const metadata = {
@@ -43,133 +39,11 @@ const About = () => {
       </section>
       {/* End Breadcrumb Sections */}
 
-      {/* Our About Area */}
-      <section className="our-about pb90">
-        <div className="container">
-          <div className="row" data-aos="fade-up" data-aos-delay="300">
-            <div className="col-lg-6">
-              <h2>
-                We&apos;re on a Mission to Change{" "}
-                <br className="d-none d-lg-block" /> View of Real Estate Field.
-              </h2>
-            </div>
-            <div className="col-lg-6">
-              <p className="text mb25">
-                It doesn’t matter how organized you are — a surplus of toys will
-                always ensure your house is a mess waiting to happen.
-                Fortunately, getting kids on board with the idea of ditching
-                their stuff is a lot easier than it sounds.
-              </p>
-              <p className="text mb55">
-                Maecenas quis viverra metus, et efficitur ligula. Nam congue
-                augue et ex congue, sed luctus lectus congue. Integer convallis
-                condimentum sem. Duis elementum tortor eget condimentum tempor.
-                Praesent sollicitudin lectus ut pharetra pulvinar.
-              </p>
-              <div className="row">
-                <Mission />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* End Our About Area */}
-
-      {/* About Banner */}
-      <section className="our-about pt-0">
-        <div className="container">
-          <div className="row" data-aos="fade-up" data-aos-delay="300">
-            <div className="col-lg-12">
-              <div className="about-page-img">
-                <Image
-                  width={1206}
-                  height={515}
-                  priority
-                  className="w-100 h-100 cover"
-                  src="/images/about/about-page-banner.png"
-                  alt="about banner"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* End About Banner */}
-
-      {/* Funfact */}
-      <section className="pt-0">
-        <div className="container">
-          <div
-            className="row justify-content-center"
-            data-aos="fade-up"
-            data-aos-delay="300"
-          >
-            <FunFact />
-          </div>
-        </div>
-      </section>
-      {/* End Funfact */}
-
-      {/* Exclusive Agents */}
-      <section className="pb90">
-        <div className="container">
-          <div className="row  justify-content-center">
-            <div className="col-auto">
-              <div
-                className="main-title"
-                data-aos="fade-up"
-                data-aos-delay="100"
-              >
-                <h2 className="title">Our Exclusive Agetns</h2>
-                <p className="paragraph">
-                  Aliquam lacinia diam quis lacus euismod
-                </p>
-              </div>
-            </div>
-            {/* End header */}
-          </div>
-          {/* End .row */}
-
-          <div className="row">
-            <div className="col-lg-12" data-aos="fade-up" data-aos-delay="300">
-              <div className="property-city-slider">
-                <Agents />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* Exclusive Agents */}
-
-      {/* Abut intro */}
-      <section className="pt30 pb-0">
-        <div className="cta-banner3 bgc-thm-light mx-auto maxw1600 pt100 pt60-lg pb90 pb60-lg bdrs24 position-relative overflow-hidden mx20-lg">
-          <div className="container">
-            <div className="row">
-              <div
-                className="col-md-6 col-lg-5 pl30-md pl15-xs"
-                data-aos="fade-left"
-                data-aos-delay="300"
-              >
-                <div className="mb30">
-                  <h2 className="title text-capitalize">
-                    Let’s find the right <br className="d-none d-md-block" />{" "}
-                    selling option for you
-                  </h2>
-                </div>
-                <div className="why-chose-list style2">
-                  <Features />
-                </div>
-                <Link href="#" className="ud-btn btn-dark">
-                  Learn More
-                  <i className="fal fa-arrow-right-long" />
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* Abut intro */}
+      {/* About page content start here */}
+      <div className="container">
+        <AboutContent />
+      </div>
+      {/* About page content end here */}
 
       {/* Our Partners */}
       <section className="our-partners">
