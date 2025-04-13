49df352c15fa5754b00944d087c8c46681057284
mdsajalali <mdsajalali2020@gmail.com>
2025-04-13 14:30:16 +0600

    added: single blog route

---
+++ src/app/(blogs)/blogs/[id]/page.js
@@ -0,0 +1,54 @@
+import CallToActions from "@/components/common/CallToActions";
+import DefaultHeader from "@/components/common/DefaultHeader";
+import Footer from "@/components/common/default-footer";
+import MobileMenu from "@/components/common/mobile-menu";
+
+export const metadata = {
+  title: "Blog  || Homez - Real Estate NextJS Template",
+};
+
+const SingleBlog = () => {
+  return (
+    <>
+      {/* Main Header Nav */}
+      <DefaultHeader />
+      {/* End Main Header Nav */}
+
+      {/* Mobile Nav  */}
+      <MobileMenu />
+      {/* End Mobile Nav  */}
+
+      {/* Breadcrumb Sections */}
+      <section className="breadcumb-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="breadcumb-style1">
+                <h2 className="title">Blog</h2>
+                <div className="breadcumb-list">
+                  <a href="/">Home</a>
+                  <a href="#">Blogs</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* End Breadcrumb Sections */}
+
+      {/* blog content content */}
+
+      {/* Our CTA */}
+      <CallToActions />
+      {/* Our CTA */}
+
+      {/* Start Our Footer */}
+      <section className="footer-style1 pt60 pb-0">
+        <Footer />
+      </section>
+      {/* End Our Footer */}
+    </>
+  );
+};
+
+export default SingleBlog;
