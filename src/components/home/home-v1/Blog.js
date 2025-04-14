c4adbd5eb5294039832a4572bf21a6825e869d67
Mahamud Pino <mahamudpino24678@gmail.com>
2025-04-15 02:22:40 +0600

    link full card

--- src/components/home/home-v1/Blog.js
+++ src/components/home/home-v1/Blog.js
@@ -7,29 +7,31 @@ const Blog = () => {
     <>
       {blogs.map((blog) => (
         <div className="col-sm-6 col-lg-4" key={blog.id}>
-          <div className="blog-style1">
-            <div className="blog-img">
-              <Image
-                width={386}
-                height={271}
-                className="w-100 h-100 cover"
-                src={blog.image}
-                alt="blog"
-              />
-            </div>
-            <div className="blog-content">
-              <div className="date">
-                <span className="month">{blog.date.month}</span>
-                <span className="day">{blog.date.day}</span>
+          <Link href={`/blogs/${blog.id}`}>
+            <div className="blog-style1">
+              <div className="blog-img">
+                <Image
+                  width={386}
+                  height={271}
+                  className="w-100 h-100 cover"
+                  src={blog.image}
+                  alt="blog"
+                />
               </div>
-              <a className="tag" href="#">
-                {blog.tag}
-              </a>
-              <h6 className="title mt-1">
-                <Link href={`/blogs/${blog.id}`}>{blog.title}</Link>
-              </h6>
+              <div className="blog-content">
+                <div className="date">
+                  <span className="month">{blog.date.month}</span>
+                  <span className="day">{blog.date.day}</span>
+                </div>
+                <a className="tag" href="#">
+                  {blog.tag}
+                </a>
+                <h6 className="title mt-1">
+                  <Link href={`/blogs/${blog.id}`}>{blog.title}</Link>
+                </h6>
+              </div>
             </div>
-          </div>
+          </Link>
         </div>
       ))}
     </>
