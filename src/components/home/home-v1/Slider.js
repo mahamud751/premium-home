5a381a0517182b0f09511e124e58f7453d1597d7
mdsajalali <mdsajalali2020@gmail.com>
2025-05-03 15:37:30 +0600

    added: slider api

--- src/components/home/home-v1/Slider.js
+++ src/components/home/home-v1/Slider.js
@@ -1,58 +1,54 @@
 "use client";
-import { useState } from "react";
-import styles from "../../../app/Slider.module.css"; // Adjust path as needed
-
-const initialItems = [
-  {
-    name: "Switzerland",
-    description:
-      "Renowned for its breathtaking Alpine scenery and precision in craftsmanship",
-    image:
-      "https://images.unsplash.com/photo-1697659902121-1ec9e3728dce?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Finland",
-    description: "Known for its saunas, lakes, and a deep connection to nature",
-    image:
-      "https://images.unsplash.com/photo-1716943958117-d69db5715ca1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Iceland",
-    description:
-      "Famous for its stunning geothermal landscapes, waterfalls, and glaciers",
-    image:
-      "https://images.unsplash.com/photo-1624715479776-c03fff721106?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Australia",
-    description:
-      "Distinguished by its diverse ecosystems, ranging from beaches to bushland",
-    image:
-      "https://plus.unsplash.com/premium_photo-1741756092543-76fef837e30b?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Netherland",
-    description:
-      "Characterized by its iconic canals, tulip fields, and windmills",
-    image:
-      "https://images.unsplash.com/photo-1618179452798-125f6706ba56?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Ireland",
-    description:
-      "Known for its lush green landscapes and rich cultural heritage",
-    image:
-      "https://images.unsplash.com/photo-1627136711221-62704f4a2cd7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-];
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styles from "../../../app/Slider.module.css";
 
 const Slider = () => {
-  const [items, setItems] = useState(initialItems);
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchBannerData = async () => {
+      try {
+        const response = await axios.get(
+          "https://premium.samironbarai.xyz/v1/banners"
+        );
+        setItems(response.data.data);
+        setLoading(false);
+      } catch (err) {
+        setError("Failed to fetch banner data");
+        setLoading(false);
+      }
+    };
+
+    fetchBannerData();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="our-faq pt-20 py-12 md:px-4">
+        <div className="container mx-auto max-w-7xl">
+          <p className="text-center ">Loading Banners...</p>
+        </div>
+      </section>
+    );
+  }
+
+  if (error) {
+    return (
+      <section className="our-faq pt-20 py-12 md:px-4">
+        <div className="container mx-auto max-w-7xl">
+          <p className="text-center text-red-600">{error}</p>
+        </div>
+      </section>
+    );
+  }
 
   const handleNext = () => {
     setItems((prevItems) => {
       const newItems = [...prevItems];
-      newItems.push(newItems.shift()); // Move first item to the end
+      newItems.push(newItems.shift());
       return newItems;
     });
   };
@@ -60,7 +56,7 @@ const Slider = () => {
   const handlePrev = () => {
     setItems((prevItems) => {
       const newItems = [...prevItems];
-      newItems.unshift(newItems.pop()); // Move last item to the start
+      newItems.unshift(newItems.pop());
       return newItems;
     });
   };
@@ -70,19 +66,31 @@ const Slider = () => {
       <div className={styles.slide}>
         {items.map((item, index) => (
           <div
-            key={item.name}
+            key={item.id}
             className={styles.item}
             style={{
-              background: `url('${item.image}')`,
+              background: `url('${item.image[0]}')`,
               backgroundSize: "cover",
               backgroundPosition: "center",
               backgroundRepeat: "no-repeat",
             }}
           >
             <div className={styles.content}>
-              <div className={styles.name}>{item.name}</div>
-              <div className={styles.description}>{item.description}</div>
-              <button>See More</button>
+              <div className="mb-4">
+                <div className="text-4xl font-semibold text-white">
+                  {item.title}
+                </div>
+                <a
+                  href={item.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-block mt-2"
+                >
+                  <button className="px-4 py-2  bg-white/40 text-black rounded-lg  hover:bg-white/60 transition-colors duration-300 shadow-md">
+                    See More
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
         ))}
