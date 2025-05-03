648450d513bce05ccce78cbd5ae2e5e3e96382cf
mdsajalali <mdsajalali2020@gmail.com>
2025-05-03 23:00:10 +0600

    update: sidebar

--- src/components/common/mobile-menu/ProSidebarContent.js
+++ src/components/common/mobile-menu/ProSidebarContent.js
@@ -1,63 +1,39 @@
 import mobileMenuItems from "@/data/mobileMenuItems";
-import { isParentActive } from "@/utilis/isMenuActive";
-import { usePathname } from "next/navigation";
 import Link from "next/link";
-
-import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
-import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { Menu, MenuItem, Sidebar } from "react-pro-sidebar";
 
 const ProSidebarContent = () => {
   const path = usePathname();
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
-
+  const handleLinkClick = () => {
+    setIsCollapsed(true);
+  };
 
   return (
-    <Sidebar width="100%" backgroundColor="#fff" className="my-custom-class">
+    <Sidebar
+      width="100%"
+      backgroundColor="#fff"
+      className="my-custom-class"
+      collapsed={isCollapsed}
+      onBackdropClick={() => setIsCollapsed(true)}
+    >
       <Menu>
         {mobileMenuItems.map((item, index) => (
-          <SubMenu
+          <MenuItem
             key={index}
-            className={isParentActive(item.subMenu, path) ? "active" : ""}
-            label={item.label}
+            component={
+              <Link
+                href={item.link}
+                className={item.link === path ? "active" : ""}
+                onClick={handleLinkClick}
+              />
+            }
           >
-            {item.subMenu.map((subItem, subIndex) =>
-              subItem.subMenu ? (
-                <SubMenu
-                  key={subIndex}
-                  label={subItem.label}
-                  className={
-                    isParentActive(subItem.subMenu, path) ? "active" : ""
-                  }
-                >
-                  {subItem.subMenu.map((nestedItem, nestedIndex) => (
-                    <MenuItem
-                      key={nestedIndex}
-                      component={
-                        <Link
-                          className={nestedItem.path == path ? "active" : ""}
-                          href={nestedItem.path}
-                        />
-                      }
-                    >
-                      {nestedItem.label}
-                    </MenuItem>
-                  ))}
-                </SubMenu>
-              ) : (
-                <MenuItem
-                  key={subIndex}
-                  component={
-                    <Link
-                      className={subItem.path == path ? "active" : ""}
-                      href={subItem.path}
-                    />
-                  }
-                >
-                  {subItem.label}
-                </MenuItem>
-              )
-            )}
-          </SubMenu>
+            {item.label}
+          </MenuItem>
         ))}
       </Menu>
     </Sidebar>
